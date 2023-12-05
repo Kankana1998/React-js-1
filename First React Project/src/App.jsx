9546456182255e8fc687e887f696eb903b2c9e1b
Kankana Nath <kankana109@gmail.com>
2023-12-05 18:33:02 +0530

    Day 2.1

--- First React Project/src/App.jsx
+++ First React Project/src/App.jsx
@@ -1,13 +1,36 @@
-
+import Card from './Card.jsx'
+import Student from './Student.jsx'
 function App() {
 
   return (
     <>
-  <h1>Vite react App</h1>
+     
+  {/*<h1 className="myclass">React is {5+6} times better with JSX</h1>;
     
-    
-    </>
-  )
+  
+  
+  React supports if statements, but not inside JSX.
+  const x = 5;
+let text = "Goodbye";
+if (x < 10) {
+  text = "Hello";
+}
+  const x = 5;
+<h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>;*/}
+
+  <Card />
+  <Card />
+  <Card />
+  <Card />
+  
+<Student name="Kankana" />
+<Student name="Alice" />
+<Student name="Bella" />
+
+
+
+  </>
+  );
 }
 
 export default App
