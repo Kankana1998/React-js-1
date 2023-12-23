c4b92e0a4af668867dfc229c62a0bf9637206567
Kankana Nath <kankana109@gmail.com>
2023-12-23 21:09:22 +0530

    Card comp

---
+++ Bro Code 2024 react course/cardComponanat/src/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+        <img className='card-image' src="https://i0.wp.com/www.thewrap.com/wp-content/uploads/2019/03/Miley-Cyrus-Hannah-Montana.jpg?w=618&ssl=1" alt="Hannah Picture" />
+        <h2>Hannah Montana</h2>
+        <p>I am a singer and song writer</p>
+    </div>
+  )
+}
+
+export default Card
