fb50ad81d86f1ba919697d44ab2187d236f71565
Kankana Nath <kankana109@gmail.com>
2023-12-26 23:23:57 +0530

    day 4

--- Bro Code 2024 react course/listRendering/src/List.jsx
+++ Bro Code 2024 react course/listRendering/src/List.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 function List() {
  
-    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    const fruits = [{id: 1, name: "apple", calories: 95}, 
+    {id: 2, name: "orange", calories: 45}, 
+    {id: 3, name: "banana", calories: 105}, 
+    {id: 4, name: "coconut", calories: 159}, 
+    {id: 5, name: "pineapple", calories: 37}];
 
     fruits.sort()
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                               {fruit.name}: &nbsp; 
+                                               <b>{fruit.calories}</b></li>);
     return(<ol>{listItems}</ol>)
 }
 
