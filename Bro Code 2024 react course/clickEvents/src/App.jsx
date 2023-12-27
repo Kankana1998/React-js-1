1921cc0b3ed9abfcd29819a60ae615b9a0323964
Kankana Nath <kankana109@gmail.com>
2023-12-27 19:31:37 +0530

    handling events 1

---
+++ Bro Code 2024 react course/clickEvents/src/App.jsx
@@ -0,0 +1,19 @@
+// Click Event - An interaction when a user clicks on a specific element. We can respond to clicks by passing a callback to the onClick event handler.
+import React from "react";
+import Button1 from "./Button1";
+import ProfilePicture from "./ProfilePicture";
+
+function App() {
+  
+
+  return (
+  <>
+  <Button1 />
+     <ProfilePicture />
+  </>
+     
+   
+  );
+}
+
+export default App
--- Bro Code 2024 react course/listRendering/src/List.jsx
+++ Bro Code 2024 react course/listRendering/src/List.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import PropTypes from 'prop-types';
 
-function List() {
+
+function List(props) {
  
-    const fruits = [{id: 1, name: "apple", calories: 95}, 
-    {id: 2, name: "orange", calories: 45}, 
-    {id: 3, name: "banana", calories: 105}, 
-    {id: 4, name: "coconut", calories: 159}, 
-    {id: 5, name: "pineapple", calories: 37}];
+   const category = props.category;
+   const itemList = props.items;
 
-    fruits.sort()
+    //fruits.sort((a,b) => b.name.localeCompare(a.name));
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>
-                                               {fruit.name}: &nbsp; 
-                                               <b>{fruit.calories}</b></li>);
-    return(<ol>{listItems}</ol>)
+    //const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
+    //const highCalFruits = fruits.filter(fruit => fruit.calories > 100)
+
+
+    const listItems = itemList.map(item => <li key={item.id}>
+                                               {item.name}: &nbsp; 
+                                               <b>{item.calories}</b></li>);
+    return(
+        <>
+        <h3 className="list-category">{category}</h3>
+        <ol className="list-items">{listItems}</ol>
+        </>
+    )
 }
 
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number, 
+                                                name: PropTypes.string,
+                                            calories: propTypes.number    })),
+}
+
+List.defaultProps = {
+    category:"Category",
+    items: [],
+}
 export default List
