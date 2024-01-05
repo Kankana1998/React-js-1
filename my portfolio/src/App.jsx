95040984c16f66890ef4f68c3e8d92432cdfc864
Kankana Nath <kankana109@gmail.com>
2024-01-05 22:08:27 +0530

    my port 2.0

--- my portfolio/src/App.jsx
+++ my portfolio/src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faBars);
+
+
+
 import './App.css'
 
 function App() {
@@ -8,7 +15,32 @@ function App() {
 
   return (
     <>
-      
+      {/* ----HEADER SECTION---- */}
+    <header>
+        <nav>
+            <div>
+                <a href="#">Kankana Nath</a>
+            </div>
+
+            <div>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#work">Work</a></li>
+                </ul>
+            </div>
+
+            <div><FontAwesomeIcon icon="fa-solid fa-bars" /></div>
+        </nav>
+    </header>
+
+
+    <main>
+        {/* ---- HOME-------------- */}
+ 
+ 
+    </main>
     </>
   )
 }
