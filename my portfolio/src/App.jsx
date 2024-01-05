b739a4e0d821c58fe39aa3982071512505702606
Kankana Nath <kankana109@gmail.com>
2024-01-06 00:25:43 +0530

    b

--- my portfolio/src/App.jsx
+++ my portfolio/src/App.jsx
@@ -11,12 +11,12 @@ library.add(faBars);
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       {/* ----HEADER SECTION---- */}
-    <header>
+    <header className="h-">
         <nav>
             <div>
                 <a href="#">Kankana Nath</a>
@@ -44,9 +44,14 @@ function App() {
             </div>
 
             <div class="home__img">
-                    <svg class="home__blob" viewBox="0 0 479 467" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-
-                    <image class="home__blob-img" x="50" y="60" href="assets/img/202011_04.jpg"/>
+                    <svg class="home__blob" viewBox="0 0 479 467" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                        <mask id="mask0" mask-type="alpha">
+                            <path d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z"/>
+                        </mask>
+                        <g mask="url(#mask0)">
+                            <path d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z"/>
+                            <image class="home__blob-img" x="50" y="60" src="src/assets/img/202011_04.jpg"/>
+                        </g>
                     </svg>
                 </div>
         </section>
@@ -57,7 +62,7 @@ function App() {
 
                 <div>
                     <div>
-                        <img src="assets/img/undraw_Web_development_0l6v.png" alt=""/>
+                        <img src="src/assets/img/undraw_Web_development_0l6v.png" alt=""/>
                     </div>
                     
                     <div>
@@ -138,7 +143,7 @@ function App() {
                     </div>
                     
                     <div>              
-                        <img src="assets/img/work3.jpg" alt="" class="skills__img"/>
+                        <img src="src/assets/img/work3.jpg" alt="" class="skills__img"/>
                     </div>
                 </div>
             </section>
@@ -149,22 +154,22 @@ function App() {
 
                 <div class="work__container bd-grid">
                     <a href="" class="work__img">
-                        <img src="assets/img/work1.jpg" alt=""/>
+                        <img src="src/assets/img/work1.jpg" alt=""/>
                     </a>
                     <a href="" class="work__img">
-                        <img src="assets/img/work2.jpg" alt=""/>
+                        <img src="src/assets/img/work2.jpg" alt=""/>
                     </a>
                     <a href="" class="work__img">
-                        <img src="assets/img/work3.jpg" alt=""/>
+                        <img src="src/assets/img/work3.jpg" alt=""/>
                     </a>
                     <a href="" class="work__img">
-                        <img src="assets/img/work4.jpg" alt=""/>
+                        <img src="src/assets/img/work4.jpg" alt=""/>
                     </a>
                     <a href="" class="work__img">
-                        <img src="assets/img/work5.jpg" alt=""/>
+                        <img src="src/assets/img/work5.jpg" alt=""/>
                     </a>
                     <a href="" class="work__img">
-                        <img src="assets/img/work6.jpg" alt=""/>
+                        <img src="src/assets/img/work6.jpg" alt=""/>
                     </a>
                 </div>
             </section>
