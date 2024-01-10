b25858585830d56af9f6c545346d5322256dd2cd
Kankana Nath <kankana109@gmail.com>
2024-01-10 21:42:20 +0530

    port

--- myPortfolio1/src/App.jsx
+++ myPortfolio1/src/App.jsx
@@ -1,13 +1,26 @@
 
 import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Header from './components/Header'
+import Main from './components/Main'
+import Navbar from './components/Navbar'
+import Services from './components/Services'
+import Project from './components/project'
 
 function App() {
   
 
   return (
-    <>
-     <h1>Hi</h1>
-    </>
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden bg-bgcolor'>
+      <Header />
+      <Navbar />
+      <Main />
+      <About />
+      <Services />
+      <Project />
+      <Contact />
+    </div>
   )
 }
 
---
+++ myPortfolio1/src/components/Project.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div>P</div>
+  )
+}
+
+export default Project
